8bd41ce5e079166a490b7727b1c90f11531b5c7e
xiaonq <1532363461@qq.com>
2021-02-12 00:45:45 +0800

    修改到github中

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -6,7 +6,7 @@ module.exports = {
   nav,
   sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: '/img/EB-logo.png', // 导航栏logo
-  repo: 'xiaonq110/vuepress-blog-xiaonq', // 导航栏右侧生成Github链接
+  repo: 'Tom7481079/vuepress-blog-xiaonq', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
   lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
   docsDir: 'docs', // 编辑的文件夹
@@ -47,7 +47,7 @@ module.exports = {
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
     name: 'xiaonaiqiang', // 必需
-    link: 'https://gitee.com/xiaonq110', // 可选的
+    link: 'https://github.com/Tom7481079/vuepress-blog-xiaonq', // 可选的
   },
   blogger: {
     // 博主信息，显示在首页侧边栏
@@ -68,7 +68,7 @@ module.exports = {
       {
         iconClass: 'icon-gitee',
         title: 'Gitee',
-        link: 'https://gitee.com/xiaonq110/',
+        link: 'https://github.com/Tom7481079/vuepress-blog-xiaonq',
       },
     ],
   },
